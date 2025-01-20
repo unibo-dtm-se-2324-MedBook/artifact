3df6db575d81b2ef7d941cdb31867302294d2320
Anastasiia <anastasii.bakhmutova@studio.unibo.it>
2025-01-20 21:07:03 +0300

    Traits are updated

--- artefact/utils/traits.py
+++ artefact/utils/traits.py
@@ -2,8 +2,11 @@
 
 base_width = 240
 base_height = 450
+txf_height = 35
+btn_width = 200
 Light_bgcolor = '#FFFAFA'
 Dark_bgcolor = '#3F888F'
+input_hint_color = '#75797c'
 #Vas = '#ABCDEF'
 b_radius = 35
-txf_height = 35
+# content_padding = padding.only(left = 20, top = 10, right = 10, botton - 10)
