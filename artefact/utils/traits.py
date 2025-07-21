38f5bfdc24b8a256953521f00a94d2da77e8bb4e
AwanRaskall <bakhmutovaaa@gmail.com>
2025-07-21 17:54:32 +0200

    Added newproperties for elements' decorations in traits.py

--- artefact/utils/traits.py
+++ artefact/utils/traits.py
@@ -7,8 +7,10 @@ btn_width = 200
 general_txt_size = 14
 txt_color_wt = 'white'
 Light_bgcolor = '#FFFAFA'
+Minor_light_bgcolor = "#FBF4EC"
 Dark_bgcolor = '#3F888F'
 input_hint_color = '#75797c'
+unit_color_dark = '#3F888F'
 #Vas = '#ABCDEF'
 b_radius = 35
 # content_padding = padding.only(left = 20, top = 10, right = 10, botton - 10)
