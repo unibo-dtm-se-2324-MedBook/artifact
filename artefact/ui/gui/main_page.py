52e7528c9ea7a6c8fe42d65905f2e414123b49dd
AwanRaskall <bakhmutovaaa@gmail.com>
2025-07-26 18:26:01 +0200

    Visual of calendar on Schedule page is done

--- artefact/ui/gui/main_page.py
+++ artefact/ui/gui/main_page.py
@@ -123,7 +123,42 @@ class MainPage(Container):
         self.prev_btn = IconButton(icons.ARROW_BACK, icon_color = unit_color_dark, on_click = self.prev_month)
         self.next_btn = IconButton(icons.ARROW_FORWARD, icon_color = unit_color_dark, on_click = self.next_month)
         
-        ## Area to add new pill
+        # Weekday headers
+        weekdays = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+        self.weekdays_row = Row(
+            spacing = 0,
+            controls = [
+                Container(
+                    width = calendar_width / 7,
+                    height = 20,
+                    alignment = alignment.center,
+                    # padding = padding.only(bottom = 2),
+                    margin = padding.only(bottom = 5),
+                    content = Text(day, size = calendar_txt, weight = FontWeight.BOLD), # text_align = 'center' 
+                    border = border.only(
+                        top=border.BorderSide(1, unit_color_dark),
+                        bottom = border.BorderSide(1, unit_color_dark)
+                    )
+                ) for day in weekdays
+            ]
+        )
+
+        # Container for the calendar with days
+        self.calendar = Container(
+            # expand=True,
+            height = calendar_height,
+            width = calendar_width,
+            border = border.only(
+                            top = border.BorderSide(1, unit_color_dark),
+                            bottom = border.BorderSide(1, unit_color_dark)
+            ),
+            padding = padding.all(0),
+            margin  = margin.all(0),
+            content = Column(spacing = 0, tight=True, controls = [])
+            # content = Column(spacing = 0, expand  = True, controls = [])
+        )
+
+        # Button to add new pill
         self.btn_add_pill = ElevatedButton(
             content = Text('Add the pill', size = 14, color = Colors.WHITE),
             height = txf_height,
@@ -132,23 +167,68 @@ class MainPage(Container):
             style = ButtonStyle(shape = RoundedRectangleBorder(radius=10)),
             on_click = self.show_form
         )
-        self.form = None
+        self.form = None 
+
 
     def set_token(self, token):
         self.token = token
         self.update()
 
-    def build(self):
 
-        # Creating a visual for Schedule page
-        # month_name = calendar.month_name[self.month]
-        # month_header = Text(f"{month_name} {self.year}", size=16)
-        self.month_header = Text()
-        self.grid = GridView(max_extent = 80, spacing = 5, run_spacing = 5)
+    def build(self):
+        # Creating a visual for timetable
+        month_header = Text(f"{calendar.month_name[self.month]} {self.year}", size = 16)
+
+        weeks = calendar.monthcalendar(self.year, self.month)
+        rows = []
+        row_height = calendar_height / 6
+        cell_width = calendar_width  / 7
+
+        for week in weeks:
+            cells = []
+            for day in week:
+                cells.append(
+                    Container(
+                        width = cell_width,
+                        height = row_height,
+                        padding = padding.all(0),
+                        margin = margin.all(0),
+                        border = border.only(
+                            top = border.BorderSide(1, unit_color_dark),
+                            bottom = border.BorderSide(1, unit_color_dark)
+                        ),
+                        content = Column(
+                            spacing = 0,
+                            # expand  = True,
+                            tight   = True,
+                            controls = [
+                                # dates
+                                Container(
+                                    content = Text(str(day) if day else '', size = calendar_txt),
+                                    alignment = alignment.center,
+                                    padding = padding.all(0),
+                                    margin = margin.all(0),
+                                ),
+                                # markers
+                                Container(
+                                    expand  = True,
+                                    padding = padding.all(0),
+                                    margin = margin.all(0),
+                                    content = None,
+                                )
+                            ]
+                        )
+                    )
+                )
+            # rows.append(Row(spacing = 0, expand  = True, controls = cells))
+            rows.append(Row(spacing = 0, tight=True, controls = cells))
+        self.calendar.content.controls = rows
+
 
         ## Combine
         schedule_content = Container(
             content = Column(
+                # expand=True,
                 controls=[
                     Row(
                         alignment = 'spaceBetween',
@@ -159,7 +239,6 @@ class MainPage(Container):
                             ),
                             Text(value = 'MedBook', weight = FontWeight.BOLD, color = 'black'),
                             Container(
-                                # padding= padding.only(right = 16), 
                                 content = Icon(name = icons.NOTIFICATIONS_OUTLINED, color = Colors.BLACK)
                             )
                         ]
@@ -167,15 +246,22 @@ class MainPage(Container):
                     Row(alignment = MainAxisAlignment.CENTER,
                         controls = [Text('Schedule', size = 16)]),
                     Row(alignment = 'spaceBetween',
-                        controls = [self.prev_btn, self.month_header, self.next_btn],
+                        controls = [self.prev_btn, month_header, self.next_btn],
                     ),
-                    self.grid,
-                    Divider(),
-                    self.btn_add_pill
+                    Column(
+                        spacing = 0,       
+                        controls = [
+                            self.weekdays_row,
+                            self.calendar,
+                        ]
+                    ),
+                    Container(
+                        # margin = padding.only(bottom = 10),
+                        content = self.btn_add_pill
+                    )
                 ]
             )
         )
-        # self.create_calendar()
 
         # Schedule page with animation characteristics
         self.schedule = Row(
@@ -187,7 +273,7 @@ class MainPage(Container):
                 border_radius = b_radius,
                 animate = animation.Animation(600, AnimationCurve.DECELERATE),
                 animate_scale = animation.Animation(400, curve = 'decelerate'),
-                padding = padding.only(top = 15, left = 20, right = 40, bottom = 5),
+                padding = padding.only(top = 15, left = 20, right = 40, bottom = 15), #5
                 clip_behavior = ClipBehavior.ANTI_ALIAS,
                 content = schedule_content
             )]
@@ -220,13 +306,50 @@ class MainPage(Container):
         self.schedule.update()
 
     # Generate calendar
-    def create_calendar(self):
-        self.grid.controls.clear()
+    # def create_calendar(self):
+    #     self.grid.controls.clear()
         
-        self.month_header.value = f'{calendar.month_name[self.month]} {self.year}'
+    #     self.month_header.value = f'{calendar.month_name[self.month]} {self.year}'
+    #     first_wday, total_days = calendar.monthrange(self.year, self.month)
+    #     for _ in range(first_wday): # empty cells before the first day
+    #         self.grid.controls.append(Container())
+
+    #     self.update()
+    
+    
+    def generate_calendar(self):
+        self.month_header.value = f"{calendar.month_name[self.month]} {self.year}"
+        self.grid.controls.clear()
+
         first_wday, total_days = calendar.monthrange(self.year, self.month)
-        for _ in range(first_wday): # empty cells before the first day
-            self.grid.controls.append(Container())
+    #     # Empty cells before first day
+        for _ in range(first_wday):
+            self.grid.controls.append(Container(width=(base_width-60)/7, height=40))
+
+    #     # Day cells
+        for day in range(1, total_days + 1):
+            # date_str = f"{self.year}-{self.month:02d}-{day:02d}"
+    #         ev_list = self.events.get(date_str, [])
+    #         # Create small dots for events
+    #         markers = [
+    #             Container(width=8, height=8, bgcolor=colors.BLUE, border_radius=4)
+    #             for _ in ev_list[:3]  # show up to 3 dots
+    #         ]
+    #         # If more than 3 events, add a '+' indicator
+    #         if len(ev_list) > 3:
+    #             markers.append(Text(f"+{len(ev_list)-3}", size=8))
+
+            day_cell = Container(
+                content=Column([
+                    Text(str(day), size=12),
+                    # Row(markers, alignment=MainAxisAlignment.CENTER)
+                ], alignment=MainAxisAlignment.START),
+                width=(base_width-60)/7,
+                height=40,
+                border=border.all(1, colors.GREY),
+                # on_click=lambda e, d=day: self.open_day_events(d)
+            )
+            self.grid.controls.append(day_cell)
 
         self.update()
 
@@ -236,14 +359,16 @@ class MainPage(Container):
             self.month, self.year = 12, self.year - 1
         else:
             self.month -= 1
-        # self.create_calendar()
+        # self.generate_calendar()
+        self.update()
 
     def next_month(self, e):
         if self.month == 12:
             self.month, self.year = 1, self.year + 1
         else:
             self.month += 1
-        # self.create_calendar()
+        # self.generate_calendar()
+        self.update()
     
     # Creating the form for new medicine
     def show_form(self, e):
@@ -267,7 +392,7 @@ class MainPage(Container):
         self.note_field = create_TextField()
 
         self.form = AlertDialog(
-            bgcolor = Minor_light_bgcolor,
+            bgcolor = minor_light_bgcolor,
             inset_padding = padding.symmetric(horizontal = 20, vertical = 20),
 
             title = Text('New Medicine'),
@@ -358,13 +483,12 @@ class MainPage(Container):
     def handle_dismissal(self, e):
         self.page.add(Text(f"DatePicker dismissed"))
         
-
     # 'Cancel' button: function to close the Form of adding new medicine
     def close_form(self):
         self.form.open = False
         self.page.update()
 
-    # Function of exit clicking the "Exit" button
+    # Function of exit clicking the "Exit" button in navigation
     def exit(self, e):
         token = self.token
         if token: 
