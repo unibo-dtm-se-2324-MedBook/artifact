027f7452511c6f0beaf289646956eba61c697fbc
AwanRaskall <bakhmutovaaa@gmail.com>
2025-07-31 18:26:15 +0200

    Rewrote save_pill_database and load_medicines_for_user functions to get key_id for medicines

--- artefact/service/database.py
+++ artefact/service/database.py
@@ -12,31 +12,40 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 
 def save_pill_database(uid, id_token, medicine, quantity, date, note):
-    db.child('users').child(uid).child('medicines').push(
-        {
+    result = db.child('users') \
+        .child(uid) \
+        .child('medicines') \
+        .push({
             'medicine_name': medicine,
             'quantity': quantity,
-            'date': date, 
+            'date': date,
             'note': note
-        },
-        id_token
-    )
+        }, id_token)
+    
+    return result['name'] # Pyrebase returns {'name': '<NEW_KEY>'}
 
 
-def load_medicines_for_user(uid, id_token, year, month):
+
+def load_medicines_for_user(uid, id_token, year, month) -> dict[str, list[dict]]:
     result = db.child('users').child(uid).child('medicines').get(id_token)
     data_by_date = {}
+    
+    if not result.each():
+        return data_by_date
 
-    if result.each():
-        for item in result.each():
-            value = item.val()
-            date_str = value['date']
-            date = datetime.strptime(date_str, '%Y-%m-%d')
-            if date.year == year and date.month == month:
-                pill = {'medicine_name': value['medicine_name'], 'quantity': value['quantity'], 'note': value['note']}
-                if date_str not in data_by_date:
-                    data_by_date[date_str] = []
-                data_by_date[date_str].append(pill)
+    for item in result.each():
+        key = item.key()
+        value = item.val()
+        date_str = value['date']
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        if date.year == year and date.month == month:
+            pill = {
+                'key': key,
+                'medicine_name': value['medicine_name'],
+                'quantity': value['quantity'],
+                'note': value['note']
+            }
+            data_by_date.setdefault(date_str, []).append(pill)
     return data_by_date
 
 
