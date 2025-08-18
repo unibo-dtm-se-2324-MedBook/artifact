eeecbd04cac0b6f4b96be1455c5c05e898b068f0
AwanRaskall <bakhmutovaaa@gmail.com>
2025-08-18 17:18:55 +0200

    Method 'on_file_picked' to activate the function to upload a file from a device by clicking 'Add new file' button

--- artefact/ui/gui/documents_page.py
+++ artefact/ui/gui/documents_page.py
@@ -3,6 +3,7 @@ from utils.traits import *
 from ui.gui.components.navigation import NavigationBar
 from ui.gui.components.page_header import PageHeader
 from service.notifications import NotificationService
+from service import documents_page_service
 
 class DocumentsPage(UserControl):
     
@@ -12,24 +13,42 @@ class DocumentsPage(UserControl):
         self.offset = transform.Offset(0,0,)
 
         self.token = ''
-        # self.user_uid = ''
+        self.user_uid = ''
+
+        self.doc_grid = GridView(
+            expand = True,
+            runs_count = 2,
+            max_extent = 260,
+            child_aspect_ratio = 1.0,
+            spacing = 10,
+            run_spacing = 10
+        )
 
         # Button to upload new file
+        self.file_picker = FilePicker(on_result = self.on_file_picked)
         self.btn_add_file = ElevatedButton(
             content = Text('Add new file', size = general_txt_size, color = Colors.WHITE),
             height = txf_height,
             width = btn_width,
             bgcolor = Dark_bgcolor,
             style = ButtonStyle(shape = RoundedRectangleBorder(radius = 10)),
-            on_click = lambda _: self.upload_file()
+            # on_click = lambda _: self.upload_file()
+            on_click = lambda _: self.file_picker.pick_files(
+                allow_multiple = False,
+                allowed_extensions = ['pdf', 'jpg', 'jpeg', 'png']
+            )
         )
 
 
     def build(self):
+        self.page.overlay.append(self.file_picker)
+
         page_header = PageHeader(current_page = None)
         
         self.token = self.page.session.get('token')
-        
+        self.user_uid = self.page.session.get('uid')
+        print('user_uid = ', self.user_uid)
+
         # Check the timer to start notification service only once
         if self.token and not self.page.session.get('reminders_started'):
             notif_service = NotificationService(self.page, self.token, page_header = page_header)
@@ -44,18 +63,14 @@ class DocumentsPage(UserControl):
                     Row(alignment = MainAxisAlignment.CENTER,
                         controls = [Text('Documents', weight = FontWeight.BOLD, size = 16)]
                     ),
-                    # Container(
-                    #     expand = True,
-                    #     padding = padding.only(top = 10, bottom = 20),
-                    #     content = Column(
-                    #         spacing = 10,
-                    #         controls = [
-                    #             row_user_name,
-                    #             row_user_surname,
-                    #             row_user_email,
-                    #         ]
-                    #     )
-                    # ),
+                    Container(
+                        expand = True,
+                        # padding = padding.only(top = 10, bottom = 20),
+                        # content = Column(
+                        # spacing = 10,
+                        # controls = [
+                        content = self.doc_grid
+                    ),
                     Container(
                         margin = padding.only(bottom = 15, top = 10), # b 20
                         content = self.btn_add_file
@@ -94,6 +109,8 @@ class DocumentsPage(UserControl):
                 controls = [navigation, self.settings]
             )
         )
+        
+        self.load_user_documents()
         return self.content
 
     # Open navigation moving the documents page to the right
@@ -104,5 +121,11 @@ class DocumentsPage(UserControl):
         self.settings.update()
 
 
-    def upload_file(self):
+    def on_file_picked(self, e: FilePickerResultEvent):
+        if e.files:
+            file_path = e.files[0].path
+            documents_page_service.upload_user_document(self.user_uid, self.token, file_path)
+            self._load_user_documents()
+
+    def _load_user_documents(self):
         pass
