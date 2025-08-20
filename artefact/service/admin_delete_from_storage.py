addb8d16c5196fadc57126f4e4cdeb307bef53c1
AwanRaskall <bakhmutovaaa@gmail.com>
2025-08-20 18:25:26 +0200

    Initialize the app through Firebase admin SDK and the function to delete file from storage

---
+++ artefact/service/admin_delete_from_storage.py
@@ -0,0 +1,27 @@
+import firebase_admin
+from firebase_admin import credentials, storage as admin_storage
+import os
+
+SERVICE_ACCOUNT_FILE = os.environ.get('SERVICE_ACCOUNT_FILE', '.secrets/service_account.json')
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(SERVICE_ACCOUNT_FILE)
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'medbook-2bed9.appspot.com'
+    })
+
+
+def delete_file_from_storage(storage_path: str):
+    try:
+        bucket = admin_storage.bucket()
+        blob = bucket.blob(storage_path)
+        
+        if not blob.exists():
+            # print(f'File already deleted or not found: {storage_path}')
+            return
+
+        blob.delete()
+        # print(f'Successfully deleted: {storage_path}')
+
+    except Exception as e:
+        print(f'Error deleting file: {e}')
