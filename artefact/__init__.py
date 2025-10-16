a0072e2144512da59ace25fac1699c7118a39fed
AwanRaskall <bakhmutovaaa@gmail.com>
2025-10-16 16:01:10 +0200

    refactor(init): use absolute imports, add main(), init pages in route_change

--- artefact/__init__.py
+++ artefact/__init__.py
@@ -1,14 +1,14 @@
 from flet import *
 
-from utils.traits import *
-from ui.gui.first_page import FirstPage
-from ui.gui.login_page import LoginPage
-from ui.gui.sign_up_page import SignUpPage
-from ui.gui.main_page import MainPage
-from ui.gui.forgot_password_page import ForgPasswPage
-from ui.gui.settings_page import SettingsPage
-from ui.gui.documents_page import DocumentsPage
-from ui.gui.medication_check_page import MedicineCheckPage
+from artefact.utils.traits import *
+from artefact.ui.gui.first_page import FirstPage
+from artefact.ui.gui.login_page import LoginPage
+from artefact.ui.gui.sign_up_page import SignUpPage
+from artefact.ui.gui.main_page import MainPage
+from artefact.ui.gui.forgot_password_page import ForgPasswPage
+from artefact.ui.gui.settings_page import SettingsPage
+from artefact.ui.gui.documents_page import DocumentsPage
+from artefact.ui.gui.medication_check_page import MedicineCheckPage
 
 class WindowDrag(UserControl):
     def __init__(self):
@@ -32,14 +32,14 @@ class App(UserControl):
         self.page = page
         self.page.spacing = 0
 
-        self.first_page = FirstPage()
-        self.login_page = LoginPage()
-        self.signup_page = SignUpPage()
-        self.main_page = MainPage()
-        self.forgpassw_page = ForgPasswPage()
-        self.settings_page = SettingsPage()
-        self.documents_page = DocumentsPage()
-        self.medicine_check_page = MedicineCheckPage()
+        # self.first_page = FirstPage()
+        # self.login_page = LoginPage()
+        # self.signup_page = SignUpPage()
+        # self.main_page = MainPage()
+        # self.forgpassw_page = ForgPasswPage()
+        # self.settings_page = SettingsPage()
+        # self.documents_page = DocumentsPage()
+        # self.medicine_check_page = MedicineCheckPage()
 
         page.on_route_change = self.route_change
         page.go("/first_page")
@@ -48,22 +48,34 @@ class App(UserControl):
         self.page.controls.clear()
 
         if self.page.route == "/first_page":
+            self.first_page = FirstPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.first_page]))
         elif self.page.route == "/login_page":
+            self.login_page = LoginPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.login_page]))
         elif self.page.route == "/passw_page":
+            self.forgpassw_page = ForgPasswPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.forgpassw_page]))
         elif self.page.route == "/signup_page":
+            self.signup_page = SignUpPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.signup_page]))
         elif self.page.route == "/main_page":
+            self.main_page = MainPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.main_page]))
         elif self.page.route == "/settings_page":
+            self.settings_page = SettingsPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.settings_page]))
         elif self.page.route == "/documents_page":
+            self.documents_page = DocumentsPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.documents_page]))
         elif self.page.route == "/pill_check_page":
+            self.medicine_check_page = MedicineCheckPage()
             self.page.add(WindowDrag(), Stack(expand=True, controls=[self.medicine_check_page]))
         
         self.page.update()
 
-app(target = App, assets_dir= 'assets')
+def main():
+    app(target = App, assets_dir = 'assets')
+
+if __name__ == "__main__":
+    main()
